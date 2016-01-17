828b1e4dd41b82ea2d77818369ae892fd8e26d79
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-01-17 07:24:08 +0500

    GA update

--- src/v1/js/counters.js
+++ src/v1/js/counters.js
@@ -1,14 +1,19 @@
 var metrica;
-var ga;
 
 (function (win, doc, tagname) {
-	ga = ga || function () {
-			(ga.q = ga.q || []).push(arguments)
-		}, ga.l = 1 * new Date();
-	var a = doc.createElement(tagname), m = doc.getElementsByTagName(tagname)[0];
-	a.async = 1;
-	a.src   = '//www.google-analytics.com/analytics.js';
-	m.parentNode.insertBefore(a, m);
+	(function (i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+		a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(win, doc, tagname, '//www.google-analytics.com/analytics.js', 'ga');
+
+	ga('create', 'UA-72511738-1', 'auto');
+	ga('send', 'pageview');
 
 	(win['yandex_metrika_callbacks'] = win['yandex_metrika_callbacks'] || []).push(function () {
 		try {
@@ -24,33 +29,29 @@ var ga;
 		}
 	});
 
-	var s = doc.createElement(tagname), n = doc.getElementsByTagName(tagname)[0];
+	var s   = doc.createElement(tagname), n = doc.getElementsByTagName(tagname)[0];
 	s.async = true;
-	s.src = 'https://mc.yandex.ru/metrika/watch.js';
+	s.src   = 'https://mc.yandex.ru/metrika/watch.js';
 	n.parentNode.insertBefore(s, n);
 })(window, document, 'script');
-
-try {
-	ga('create', 'UA-72511738-1', 'auto');
-	ga('require', 'linkid');
-	ga('send', 'pageview');
-} catch (e) {}
 
 exports.track = function (page, value) {
 	try {
 		ga('send', 'event', {
 			'eventCategory': page,
-			'eventAction': 'Track',
-			'eventValue': value
+			'eventAction'  : 'Track',
+			'eventValue'   : value
 		});
 		metrica && metrica.hit(`?${page}`, {
-			player  : value
+			player: value
 		});
-	} catch (e) {}
+	} catch (e) {
+	}
 };
 
 exports.goal = function (id, opts) {
 	try {
 		metrica && metrica.reachGoal(id, opts);
-	} catch (e) {}
+	} catch (e) {
+	}
 };
